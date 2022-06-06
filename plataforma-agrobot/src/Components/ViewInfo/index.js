eb96e1ad66df635c88930641c3f901d4afa076e1
Lucas <lucasgrafimar@gmail.com>
2022-06-06 10:04:36 -0300

    create ViewInfo component

---
+++ plataforma-agrobot/src/Components/ViewInfo/index.js
@@ -0,0 +1,61 @@
+import { useState } from "react";
+import { MdDownload } from "react-icons/md";
+import "./style.scss";
+
+function ViewInfo({
+  isLarge = true,
+  titleView = "Pulverizar Plantação Região Leste",
+  numericField = 0,
+  FirstField = "Delivery",
+  SecondField = "20/03/2000",
+}) {
+  return (
+    <div id="container-box" className={isLarge ? "large" : "small"}>
+      <div id="box-top">
+        <h1>{titleView}</h1>
+      </div>
+      <div id="box-bottom">
+        {isLarge ? (
+          <>
+            <div id="content-1">
+              <h2>N° de Missões: {numericField}</h2>
+            </div>
+            <div id="content-2">
+              <div id="container-text">
+                <h3>Tipo: </h3>
+                <h4>{FirstField}</h4>
+              </div>
+              <div id="container-text">
+                <h3>Data: </h3>
+                <h4>{SecondField}</h4>
+              </div>
+            </div>
+          </>
+        ) : (
+          <>
+            <div id="content-1">
+              <h2>N° de Pontos: {numericField}</h2>
+            </div>
+            <div id="content-2">
+              <div id="container-text">
+                <h3>Ordem: </h3>
+                <h4>{FirstField}</h4>
+              </div>
+            </div>
+          </>
+        )}
+        {isLarge ? (
+          <div id="content-3">
+            <div id="icon">
+              <MdDownload size={16} color="#1C1B1F" />
+            </div>
+          </div>
+        ) : (
+          <div id="content-3" />
+        )}
+      </div>
+    </div>
+  );
+}
+
+export default ViewInfo;
--- plataforma-agrobot/src/routes.js
+++ plataforma-agrobot/src/routes.js
@@ -1,13 +1,14 @@
 import ReactDOM from "react-dom/client";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import DrawNavigation from "./Components/DrawNavigation";
+import ViewInfo from "./Components/ViewInfo";
 
 function App() {
   return (
     <div>
       <BrowserRouter>
         <Routes>
-          <Route path="/" element={<DrawNavigation />} />
+          <Route path="/" element={<ViewInfo />} />
           {/* <Route path="expenses" element={<Expenses />} />
           <Route path="invoices" element={<Invoices />} /> */}
         </Routes>
